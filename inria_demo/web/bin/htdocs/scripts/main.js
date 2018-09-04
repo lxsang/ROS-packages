5a79f509305f6462f8ceaa12b3dba4053f39e77b
Xuan Sang LE <sang.le@gmail.com>
2018-09-04 17:26:38 +0200

    remove Promise

--- inria_demo/web/bin/htdocs/scripts/main.js
+++ inria_demo/web/bin/htdocs/scripts/main.js
@@ -1,6 +1,7 @@
 
-VIZ = {
-    uri: 'ws://10.1.160.205:9090',
+
+var VIZ = {
+    uri: "ws://10.1.160.205:9090",
     handles: {},
     canvas: null,
     buffer: null,
@@ -15,16 +16,24 @@ VIZ = {
     events: {},
     scale: 1.0,
     draw: function () {
-        VIZ.unbindAll()
+        VIZ.unbindAll();
         if (VIZ.msgs.map)
-            VIZ.drawMap(VIZ.msgs.map)
+        {
+            VIZ.drawMap(VIZ.msgs.map);
+        }
         if (VIZ.msgs.path)
-            VIZ.drawPath(VIZ.msgs.path)
+        {
+            VIZ.drawPath(VIZ.msgs.path);
+        }
         if (VIZ.msgs.pose)
-            VIZ.drawRobot(VIZ.msgs.pose)
+        {
+            VIZ.drawRobot(VIZ.msgs.pose);
+        }
         if (VIZ.msgs.goals)
-            VIZ.drawGoals(VIZ.msgs.goals)
-        VIZ.changed = true
+        {
+            VIZ.drawGoals(VIZ.msgs.goals);
+        }
+        VIZ.changed = true;
     },
     bind: function (name, obj) {
         if (!VIZ.events[name]) {
@@ -41,112 +50,114 @@ VIZ = {
                     var top = box.y * VIZ.scale;
                     var right = (box.x + box.w) * VIZ.scale;
                     var bottom = (box.y + box.h) * VIZ.scale;
-                    return (p.x > left && p.y > top && p.x < right && p.y < bottom);
-                }
-                //console.log(mouse);
-                for (i = 0; i < VIZ.events[name].length; i++) {
+                    return (p.x > left && p.y > top &&
+                            p.x < right && p.y < bottom);
+                };
+                for (var i = 0; i < VIZ.events[name].length; i+=1) {
                     if (hit(mouse, VIZ.events[name][i].box)) {
-                        VIZ.events[name][i].handle(e)
+                        VIZ.events[name][i].handle(e);
                     }
                 }
 
-            })
+            });
         }
-        VIZ.events[name].push(obj)
+        VIZ.events[name].push(obj);
     },
     unbindAll: function () {
-        for (k in VIZ.events)
-            $(VIZ.canvas).unbind(k)
-        VIZ.events = {}
+        for(var k in VIZ.events)
+        {
+            $(VIZ.canvas).unbind(k);
+        }
+        VIZ.events = {};
     },
-    ready: function () {
+    ready: function (resolve, reject) {
+        
         //subscriber to topic map
-        return new Promise(function (resolve, reject) {
+        //return new Promise(function (resolve, reject) {
             var ros = new ROSLIB.Ros({
                 url: VIZ.uri
             });
 
-            ros.on('connection', function () {
-                alertify.success('Connected to websocket server.');
+            ros.on("connection", function () {
+                alertify.success("Connected to websocket server.");
                 resolve(ros);
             });
 
-            ros.on('error', function (error) {
-                reject('Error connecting to websocket server: ' + VIZ.uri);
+            ros.on("error", function (error) {
+                reject("Error connecting to websocket server: " + VIZ.uri);
             });
 
-            ros.on('close', function () {
-                alertify.warning('Connection to websocket server closed.');
+            ros.on("close", function () {
+                alertify.warning("Connection to websocket server closed.");
             });
-
-        });
+            
+        //});
     },
-    init: function () {
-        return new Promise(function (resolve, reject) {
-            VIZ.ready()
-                .then(function (ros) {
+    init: function (resolve, reject) {
+        //return new Promise(function (resolve, reject) {
+            VIZ.ready(function (ros) {
                     VIZ.handles.maplistener = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/map',
-                        messageType: 'nav_msgs/OccupancyGrid',
+                        name: "/map",
+                        messageType: "nav_msgs/OccupancyGrid",
                         throttle_rate: VIZ.rate
                     });
 
                     VIZ.handles.maplistener.subscribe(function (message) {
-                        VIZ.msgs.map = message
-                        VIZ.draw()
+                        VIZ.msgs.map = message;
+                        VIZ.draw();
                         //listener.unsubscribe();
                         //ros.close()
                     });
 
                     VIZ.handles.pathlistener = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/move_base/GlobalPlanner/plan',
-                        messageType: 'nav_msgs/Path',
+                        name: "/move_base/GlobalPlanner/plan",
+                        messageType: "nav_msgs/Path",
                         throttle_rate: VIZ.rate
                     });
                     VIZ.handles.pathlistener.subscribe(function (msg) {
-                        VIZ.msgs.path = msg
-                        VIZ.draw()
+                        VIZ.msgs.path = msg;
+                        VIZ.draw();
                     });
 
                     VIZ.handles.poselistener = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/robot_pose',
-                        messageType: 'geometry_msgs/Pose',
+                        name: "/robot_pose",
+                        messageType: "geometry_msgs/Pose",
                         throttle_rate: VIZ.rate
                     });
 
                     VIZ.handles.poselistener.subscribe(function (msg) {
                         //console.log(msg)
-                        VIZ.msgs.pose = msg
-                        VIZ.draw()
+                        VIZ.msgs.pose = msg;
+                        VIZ.draw();
                     });
 
                     VIZ.handles.goallistener = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/robotcmd/goals',
-                        messageType: 'geometry_msgs/PoseArray',
+                        name: "/robotcmd/goals",
+                        messageType: "geometry_msgs/PoseArray",
                         throttle_rate: VIZ.rate
                     });
 
                     VIZ.handles.goallistener.subscribe(function (msg) {
                         //console.log(msg)
-                        VIZ.msgs.goals = msg
+                        VIZ.msgs.goals = msg;
                         //console.log(msg)
-                        VIZ.draw()
+                        VIZ.draw();
                     });
 
                     VIZ.handles.goalstatus = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/move_base/status',
-                        messageType: 'actionlib_msgs/GoalStatusArray',
+                        name: "/move_base/status",
+                        messageType: "actionlib_msgs/GoalStatusArray",
                         throttle_rate: VIZ.rate
                     });
                     VIZ.handles.next_goal = new ROSLIB.Topic({
                         ros: ros,
-                        name: '/robotcmd/next_goal',
-                        messageType: 'std_msgs/Bool'
+                        name: "/robotcmd/next_goal",
+                        messageType: "std_msgs/Bool"
                     });
                     VIZ.handles.goalstatus.subscribe(function (msg) {
                         //inform the user to go to next
@@ -154,70 +165,70 @@ VIZ = {
                         if (!VIZ.msgs.goals || VIZ.msgs.goals.poses.length == 0) return;
                         if(VIZ.msgcount > 0) { VIZ.msgcount--; return;  }
                         var busy = false;
-                        for (i = 0; i < msg.status_list.length; i++) {
+                        for (var i = 0; i < msg.status_list.length; i++) {
                             if (msg.status_list[i].status != 3)
                                 busy = true;
                         }
                         if (busy) return;
                         VIZ.lock = true;
-                        alertify.alert().setting('modal', false);
-                        alertify.alert('Closable: false').set('closable', false); 
-                        alertify.alert('Go next', 'Navigate to the next goal', function(){ 
+                        alertify.alert().setting("modal", false);
+                        alertify.alert("Closable: false").set("closable", false); 
+                        alertify.alert("Go next", "Navigate to the next goal?", function(){ 
                             var msg = new ROSLIB.Message({data: true});
                             VIZ.handles.next_goal.publish(msg);
                             VIZ.lock = false;
                             VIZ.msgcount = 10;
-                            alertify.success('Going to the next goal');
+                            alertify.success("Going to the next goal");
                         });
 
                     });
 
-                    resolve()
-                })
-                .catch(function (e) {
-                    reject(e)
-                })
-        });
+                    resolve();
+                },
+                function (e) {
+                    reject(e);
+                });
+        //});
     },
     main: function () {
-        VIZ.init().then(VIZ.update).catch(function (e) {
-            alertify.error(e)
+        VIZ.init(VIZ.update, function (e) {
+            alertify.error(e);
             alertify.prompt("Please enter the correct Uri", VIZ.uri,
                 function (evt, value) {
                     VIZ.uri = value;
-                    VIZ.main()
+                    VIZ.main();
                 },
                 function () {
-                    alertify.error('Abort the connection');
+                    alertify.error("Abort the connection");
                 })
                 ;
-        })
+        });
     },
-    robotPose2Canvas(p) {
+  robotPose2Canvas: function(p) {
         var center_x = VIZ.origin.x + p.x / VIZ.resolution;
         var center_y = VIZ.origin.y - p.y / VIZ.resolution;
-        return { x: center_x, y: center_y }
+        return { x: center_x, y: center_y };
     },
     drawGoals: function (msg) {
         //console.log("draw goal");
         var canvas = VIZ.buffer;
-        var context = canvas.getContext('2d');
+        var context = canvas.getContext("2d");
         var radius = 7;
 
-        for (i = 0; i < msg.poses.length; i++) {
+        for (var i = 0; i < msg.poses.length; i++) {
             var pose = VIZ.robotPose2Canvas(msg.poses[i].position);
             context.moveTo(pose.x, pose.y);
             context.beginPath();
             //console.log(radius);
             context.arc(pose.x, pose.y, radius, 0, 2 * Math.PI, false);
-            context.fillStyle = '#ea5e07';
+            context.fillStyle = "#ea5e07";
             context.fill();
             context.lineWidth = 1;
-            context.strokeStyle = '#262a30';
+            context.strokeStyle = "#262a30";
             context.stroke();
             context.font = "bold 10pt arial";
-            context.strokeStyle = 'white';
-            context.fillStyle = 'white';
+            context.strokeStyle = "white";
+            context.fillStyle = "white";
             context.textAlign = "center";
             context.fillText(i + 1, pose.x, pose.y + 3.5);
 
@@ -230,15 +241,15 @@ VIZ = {
                     h: radius * 2
                 },
                 handle: function (e) {
-                    alertify.message("User clicked on goal " + (i + 1))
+                    alertify.message("User clicked on goal " + (i + 1));
                 }
-            })
+            });
         }
     },
     drawPath: function (msg) {
         if (msg.poses.length == 0) return;
         var canvas = VIZ.buffer;
-        var ctx = canvas.getContext('2d');
+        var ctx = canvas.getContext("2d");
         var i = 0;
         ctx.beginPath();
         while (i < msg.poses.length - 1) {
@@ -250,22 +261,22 @@ VIZ = {
             ctx.lineTo(pose.x, pose.y);
             i++;
         }
-        ctx.strokeStyle = '#F48042';
+        ctx.strokeStyle = "#F48042";
         ctx.stroke();
     },
     drawRobot: function (msg) {
         var canvas = VIZ.buffer;
-        var context = canvas.getContext('2d');
+        var context = canvas.getContext("2d");
         var pose = VIZ.robotPose2Canvas(msg.position);
 
         context.beginPath();
         var radius = VIZ.robotradius / VIZ.resolution / 2;
         //console.log(radius);
         context.arc(pose.x, pose.y, radius, 0, 2 * Math.PI, false);
-        context.fillStyle = 'green';
+        context.fillStyle = "green";
         context.fill();
         context.lineWidth = 1;
-        context.strokeStyle = '#003300';
+        context.strokeStyle = "#003300";
         context.stroke();
     },
     drawMap: function (msg) {
@@ -275,7 +286,7 @@ VIZ = {
         VIZ.resolution = msg.info.resolution;
         VIZ.origin.x = -msg.info.origin.position.x / msg.info.resolution;
         VIZ.origin.y = canvas.height + msg.info.origin.position.y / msg.info.resolution;
-        var ctx = canvas.getContext('2d');
+        var ctx = canvas.getContext("2d");
         // first, create a new ImageData to contain our pixels
         var imgData = ctx.createImageData(msg.info.width, msg.info.height); // width x height
         var data = imgData.data;
@@ -317,7 +328,7 @@ VIZ = {
         var height = VIZ.buffer.height * VIZ.scale;
         canvas.width = width;
         canvas.height = height;
-        var ctx = canvas.getContext('2d');
+        var ctx = canvas.getContext("2d");
         ctx.save();
         //ctx.translate(-((newWidth-width)/2), -((newHeight-height)/2));
         ctx.scale(VIZ.scale, VIZ.scale);
@@ -330,34 +341,35 @@ VIZ = {
     zoom: function (delta) {
         VIZ.scale += 0.2 * delta;
     }
-}
+};
+
 
 var isFullscreen = false;
 
 var enterFullscreen = function () {
-    var el = $("body")[0]
+    var el = $("body")[0];
     if( isFullscreen )
     {
-        isFullscreen = false
+        isFullscreen = false;
         if(document.exitFullscreen)
-            return document.exitFullscreen()
+            return document.exitFullscreen();
         if(document.mozCancelFullScreen)
-            return document.mozCancelFullScreen() 
+            return document.mozCancelFullScreen() ;
         if(document.webkitExitFullscreen)   
-            return document.webkitExitFullscreen()
+            return document.webkitExitFullscreen();
         if(document.cancelFullScreen)
-            return document.cancelFullScreen()
+            return document.cancelFullScreen();
     }
     isFullscreen = true;
     if (el.requestFullscreen)
-        return el.requestFullscreen()
+        return el.requestFullscreen();
     else if (el.mozRequestFullScreen)
-        return el.mozRequestFullScreen()
+        return el.mozRequestFullScreen();
     else if (el.webkitRequestFullscreen)
-        return el.webkitRequestFullscreen()
+        return el.webkitRequestFullscreen();
     else if (el.msRequestFullscreen)
-        return el.msRequestFullscreen()
-}
+        return el.msRequestFullscreen();
+};
 window.mobilecheck = function () {
     if (navigator.userAgent.match(/Android/i)
         || navigator.userAgent.match(/webOS/i)
@@ -373,33 +385,114 @@ window.mobilecheck = function () {
         return false;
     }
 };
-/*var initConsole = function () {
 
-    if (!mobileConsole.status.initialized) {
-        mobileConsole.init();
-    }
-}*/
-$(window).on('load', function () {
-    //if (mobilecheck())
-    //    initConsole()
+var initAndroidWebSocket = function()
+{
 
+        if(! window.WebSocketFactory) return;
+        // window object
+        var global = window;
+        
+        // WebSocket Object. All listener methods are cleaned up!
+        var WebSocket = global.WebSocket = function(url) {
+            // get a new websocket object from factory (check com.strumsoft.websocket.WebSocketFactory.java)
+            this.socket = WebSocketFactory.getInstance(url);
+            // store in registry
+            if(this.socket) {
+                WebSocket.store[this.socket.getId()] = this;
+            } else {
+                throw new Error("Websocket instantiation failed! Address might be wrong.");
+            }
+        };
+        
+        // storage to hold websocket object for later invokation of event methods
+        WebSocket.store = {};
+        
+        // static event methods to call event methods on target websocket objects
+        WebSocket.onmessage = function (evt) {
+            WebSocket.store[evt._target]["onmessage"].call(global, evt);
+        }	
+        
+        WebSocket.onopen = function (evt) {
+            WebSocket.store[evt._target]["onopen"].call(global, evt);
+        }
+        
+        WebSocket.onclose = function (evt) {
+            WebSocket.store[evt._target]["onclose"].call(global, evt);
+        }
+        
+        WebSocket.onerror = function (evt) {
+            WebSocket.store[evt._target]["onerror"].call(global, evt);
+        }
+    
+        // instance event methods
+        WebSocket.prototype.send = function(data) {
+            this.socket.send(data);
+        }
+    
+        WebSocket.prototype.close = function() {
+            this.socket.close();
+        }
+        
+        WebSocket.prototype.getReadyState = function() {
+            this.socket.getReadyState();
+        }
+        ///////////// Must be overloaded
+        WebSocket.prototype.onopen = function(){
+            throw new Error("onopen not implemented.");
+        };
+        
+        // alerts message pushed from server
+        WebSocket.prototype.onmessage = function(msg){
+            throw new Error("onmessage not implemented.");
+        };
+        
+        // alerts message pushed from server
+        WebSocket.prototype.onerror = function(msg){
+            throw new Error("onerror not implemented.");
+        };
+        
+        // alert close event
+        WebSocket.prototype.onclose = function(){
+            throw new Error("onclose not implemented.");
+        };
+};
+
+var tesws = function()
+{
+    var websocket = new WebSocket("wss://echo.websocket.org"); 
+	websocket.onopen = function(evt) {alertify.success("Ws connected");websocket.send("test data"); }; 
+	websocket.onclose = function(evt) { alertify.warning("Ws close"); }; 
+	websocket.onmessage = function(evt) { alertify.message("Ws msg" + evt.data); websocket.close(); }; 
+	websocket.onerror = function(evt) { alertify.error("ERROR: Ws problem"); }; 
+    alertify.message("End Init the websocket");
+    
+};
+
+var boot = function()
+{
+    initAndroidWebSocket();
+    
     $("#fullscreenbt").click(function (e) {
-        enterFullscreen()
-    })
+        enterFullscreen();
+    });
     $("#zoomin").click(function (e) {
-        VIZ.zoom(1)
-    })
+        VIZ.zoom(1);
+    });
     $("#zoomout").click(function (e) {
-        VIZ.zoom(-1)
-    })
+        VIZ.zoom(-1);
+    });
     // mouse event
     var hamster = Hamster($("#canvas")[0]);
     hamster.wheel(function (event, delta, deltaX, deltaY) {
-        VIZ.zoom(delta)
+        VIZ.zoom(delta);
     });
 
     VIZ.buffer = $("<canvas>")[0];
     VIZ.canvas = $("#canvas")[0];
     alertify.defaults.glossary.title = "VIZ";
-    VIZ.main()
-});
+    VIZ.main();
+    //tesws()
+
+};
+$(window).on("load", boot);
